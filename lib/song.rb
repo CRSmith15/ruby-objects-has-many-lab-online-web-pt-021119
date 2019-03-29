fb406cc413a7d9b6a6a05a8d9c696a99f06f5ad4
Chance Smith <chancesmith97@hotmail.com>
2019-03-29 01:32:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,16 @@
 class Song 
   attr_accessor :artist, :name, :genre 
-  
+  @@all = []
   def initialize(name)
     @name = name 
-    @@all = 
+    @@all << name
+    name.artist = 
   end 
+  
+  def self.all 
+    @@all
+  end
+  
+  
   
 end
