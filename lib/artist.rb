f38c09420e50eec66b5a9549a416e758f66e58fa
Chance Smith <chancesmith97@hotmail.com>
2019-03-29 01:20:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,14 +5,16 @@ class Artist
     @name = name 
     @songs = []
   end
-  
-  def songs 
-    @songs 
-  end
+ 
   
   def add_song(song)
     @song << song
     song.artist = self 
+  end
+  
+   
+  def songs 
+    @songs 
   end
   
   def add_song_by_name(name)
